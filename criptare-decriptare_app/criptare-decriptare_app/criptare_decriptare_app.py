696184a6e6e954c33801abd330a8fce3edac2ae4
sorina299 <64218527+sorina299@users.noreply.github.com>
2020-08-25 20:49:36 +0300

    0.0.1

---
+++ criptare-decriptare_app/criptare-decriptare_app/criptare_decriptare_app.py
@@ -0,0 +1,68 @@
+
+class Reader :
+
+    def read_text (self):
+
+        self.text = input('Enter the text: ')
+        self.key = input('Enter the keyword: ')
+
+    def print(self):
+        print("Text: " + self.text +  " Key: " + self.key)
+
+
+#######
+
+#class Menu :
+
+    def __init__(self):
+        self.print_menu()
+
+
+    def print_menu(self):
+
+    def start_encryption(self):
+
+    def start_decryption(self):
+
+##########
+
+reader = Reader()
+reader.read_text()
+reader.print()
+
+
+
+
+
+
+#r1 = Reader()
+#r2 = Reader()
+
+#r1.key = "banane"
+##r2 = r1
+#r2.key = r1.key
+#r1.key = "pere"
+
+
+#nr = str(43)
+
+#print( "ana are : " + str(nr) + " mere" )
+
+
+#print("R1:")
+#r2.print()
+
+#print("R2:")
+#r1.print()
+
+
+
+
+
+
+
+
+
+
+
+
